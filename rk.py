25a5e98367321a2032bf06d190d2ad515759f95a
Giovanni Guarnieri <giovanniguarnieri@id.uff.br>
2017-11-14 15:59:45 -0200

    virou algo do demo

--- rk.py
+++ rk.py
@@ -8,7 +8,9 @@ filewrite=open(sys.argv[1],'w')
 
 
 def func(t,y):
- return 1+(t-y)**2
+ return y 
+def func2(t,z,y):
+ return 3*z-2*y+6*np.exp(-t)
 
 def k2(t,y):
  return func(t+0.5*h,y+0.5*h*func(t,y))
@@ -17,17 +19,29 @@ def k3(t,y):
 def k4(t,y):
  return func(t+h,y+h*k3(t,y))
 
-yini=1.
-tini=2.
-tmax=3.
+def g2(t,z,y):
+ return func2(t+0.5*h,z+0.5*h*func2(t,z,y),y+0.5*h*func(t,y))
+def g3(t,z,y):
+ return func2(t+0.5*h,z+0.5*h*func2(t,z,y)*g2(t,z,y),y+0.5*h*func(t,y)*k2(t,y))
+def g4(t,z,y):
+ return func2(t+h,z+h*g3(t,z,y),y+h*k3(t,y))
+
+
+yini=2.
+zini=2.
+tini=0.
+tmax=1.
 N=10.
 h=(tmax-tini)/N
-
 t=tini
 y=yini
+z=zini
+
 filewrite.write(str(t) + '     ' + str(y) + '\n')
 while(t<tmax):
+ oy=y
  y=y+h/6*(func(t,y)+2*k2(t,y)+2*k3(t,y)+k4(t,y))
+ z=z+h/6*(func2(t,z,y)+2*g2(t,z,y)+2*g3(t,z,y)+g4(t,z,y))
  t=t+h
- filewrite.write(str(t) + '     ' + str(y) + '\n')
+ filewrite.write(str(t) + '     ' + str(y) + '     ' + str(z) +'\n')
 
