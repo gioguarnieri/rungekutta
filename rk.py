0d612d717e362679981516b67252682f975f6bd9
Giovanni Guarnieri <giovanniguarnieri@id.uff.br>
2017-11-14 15:30:39 -0200

    Pequena correção

--- rk.py
+++ rk.py
@@ -7,15 +7,15 @@ import sys
 filewrite=open(sys.argv[1],'w')
 
 
-def func(y,t):
+def func(t,y):
  return 1+(t-y)**2
 
-def k2(y,t):
- return func(t+0.5*h,y+0.5*h*func(y,t))
-def k3(y,t):
- return func(t+0.5*h,y+0.5*h*k2(y,t))
-def k4(y,t):
- return func(t+h,y+h*k3(y,t))
+def k2(t,y):
+ return func(t+0.5*h,y+0.5*h*func(t,y))
+def k3(t,y):
+ return func(t+0.5*h,y+0.5*h*k2(t,y))
+def k4(t,y):
+ return func(t+h,y+h*k3(t,y))
 
 yini=1.
 tini=2.
@@ -27,7 +27,7 @@ t=tini
 y=yini
 filewrite.write(str(t) + '     ' + str(y) + '\n')
 while(t<tmax):
- y=y+h/6*(func(y,t)+2*k2(y,t)+2*k3(y,t)+k4(y,t))
+ y=y+h/6*(func(t,y)+2*k2(t,y)+2*k3(t,y)+k4(t,y))
  t=t+h
  filewrite.write(str(t) + '     ' + str(y) + '\n')
 
