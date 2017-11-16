f8adff0014955f877212dc811466e5d6ec5c4eed
Giovanni Guarnieri <giovanniguarnieri@id.uff.br>
2017-11-16 15:59:24 -0200

    socorro

--- rk.py
+++ rk.py
@@ -10,7 +10,7 @@ filewrite=open(sys.argv[1],'w')
 def func(t,y):
  return y 
 def func2(t,z,y):
- return (3*z-2*y+6*np.exp(-t))
+ return (3.*z-2.*y+6.*np.exp(-t))
 
 def k1(t,y):
  return h*func(t,y)
@@ -20,17 +20,17 @@ def g1(t,z,y):
 def k2(t,y):
  return h*func(t+0.5*h,y+0.5*k1(t,y))
 def g2(t,z,y):
- return h*func2(t+0.5*h,z+0.5*g1(t,z,y),y+0.5*k1(t,y))
+ return h*func2(t+0.5*h,y+0.5*g1(t,y,z),z+0.5*k1(t,z))
 
 def k3(t,y):
  return h*func(t+0.5*h,y+0.5*k2(t,y))
 def g3(t,z,y):
- return h*func2(t+0.5*h,z+0.5*g2(t,z,y),y+0.5*k2(t,y))
+ return h*func2(t+0.5*h,z+0.5*g2(t,y,z),y+0.5*k2(t,z))
 
 def k4(t,y):
  return h*func(t+h,y+k3(t,y))
 def g4(t,z,y):
- return h*func2(t+h,z+g3(t,z,y),y+k3(t,y))
+ return h*func2(t+h,z+g3(t,y,z),y+k3(t,z))
 
 
 yini=2.
@@ -47,7 +47,7 @@ z=zini
 
 while(t<tmax):
  filewrite.write(str(t) + '     ' + str(y) + '     ' + str(z) +'\n')
- y=y+1./6*(k1(t,y)+2*k2(t,y)+2*k3(t,y)+k4(t,y))
- z=z+1./6*(g1(t,z,y)+2*g2(t,z,y)+2*g3(t,z,y)+g4(t,z,y))
+ y=y+1./6*(k1(t,z)+2.*k2(t,z)+2.*k3(t,z)+k4(t,z))
+ z=z+1./6*(g1(t,z,y)+2.*g2(t,z,y)+2.*g3(t,z,y)+g4(t,z,y))
  t=t+h
 
